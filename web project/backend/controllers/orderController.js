c65706e1b1b206b36a942f72cce4433a4243bba1
Shetiah21 <hatemhussien91@gmail.com>
2025-06-11 04:28:12 +0300

    Add files via upload

---
+++ web project/backend/controllers/orderController.js
@@ -0,0 +1,16 @@
+// Order controller functions
+export const createOrder = (req, res) => {
+    res.send('Create order controller');
+};
+
+export const getUserOrders = (req, res) => {
+    res.send('Get user orders controller');
+};
+
+export const getOrderById = (req, res) => {
+    res.send('Get order by ID controller');
+};
+
+export const updateOrderStatus = (req, res) => {
+    res.send('Update order status controller');
+}; 
